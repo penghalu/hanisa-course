ac42eb5a1638a98f6fdf7c42b921e5a99b44abd7
Madyan <madyan.eka95@gmail.com>
2019-07-02 08:05:41 +0700

    using the CustomUser model instead the original one

--- hanisa_project/settings.py
+++ hanisa_project/settings.py
@@ -123,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Using custom user model instead of the built-in one
+AUTH_USER_MODEL = 'users.CustomUser'
