4695355a7bc5d0f9d43d680a88d9230c6245dcd0
Madyan <madyan.eka95@gmail.com>
2019-07-02 07:08:04 +0700

    PostgreSQL configuration

--- hanisa_project/settings.py
+++ hanisa_project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'hanisa_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hanisa-course',
+        'USER': 'dbadmin',
+        'PASSWORD': 'dbadmin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
