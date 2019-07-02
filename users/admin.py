8b49a61a9a0ad7d5dd0fb90ed5b9e63ac03a1280
Madyan <madyan.eka95@gmail.com>
2019-07-02 22:24:50 +0700

    display all fields from CustomUser model on admin page (creation)

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm
 from .models import CustomUser
 
 
 # Register your models here.
-admin.site.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('gender', 'address', 'phone', 'description', 'role')}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
