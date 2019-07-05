ed70d736e40fb861c422700b757329d58c548f55
Madyan <madyan.eka95@gmail.com>
2019-07-05 15:15:20 +0700

    school list UI

--- course_management/urls.py
+++ course_management/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import SubjectAdd, SubjectList, StudentAdd, StudentList
+from .views import SchoolList, SubjectAdd, SubjectList, StudentAdd, StudentList
 
 
 app_name = 'course_management'
@@ -10,4 +10,5 @@ urlpatterns = [
     path('subjects/add/', SubjectAdd.as_view(), name='subject_add'),
     path('students/', StudentList.as_view(), name='student_list'),
     path('students/add/', StudentAdd.as_view(), name='student_add'),
+    path('schools/', SchoolList.as_view(), name='school_list'),
 ]
--- course_management/views.py
+++ course_management/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import reverse
 from django.views.generic import CreateView, ListView
 
-from .models import Student, Subject
+from .models import School, Student, Subject
 
 
 # Create your views here.
@@ -39,3 +39,10 @@ class StudentAdd(CreateView):
 
     def get_success_url(self):
         return reverse('course_management:student_list')
+
+
+class SchoolList(LoginRequiredMixin, ListView):
+    model = School
+    context_object_name = 'schools'
+    template_name = 'course_management/school_list.html'
+    login_url = 'home'
