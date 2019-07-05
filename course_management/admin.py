fc838c06b52f6de92f05aeac792c406c50f98eab
Madyan <madyan.eka95@gmail.com>
2019-07-05 17:14:36 +0700

    register course_management app models to django admin

--- course_management/admin.py
+++ course_management/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import School, Student, Subject
+
+
 # Register your models here.
+admin.site.register(School)
+admin.site.register(Student)
+admin.site.register(Subject)
