f02ac45538a6874ae97f81bc695c3f4ac1d235c8
Madyan <madyan.eka95@gmail.com>
2019-07-05 13:28:13 +0700

    New models: Subject, Student, School

--- course_management/models.py
+++ course_management/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+
 # Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=20, verbose_name='Subject')
+    code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=200, blank=True)
+    grade = models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.name
+
+
+class School(models.Model):
+    name = models.CharField(max_length=50)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='school')
+
+    def __str__(self):
+        return self.name
